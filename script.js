6bc4721052e0c5ef8cc271dde6d1753641156f1a
Jefferson Augusto Ribeiro <74573131+jefferson-ribeiro@users.noreply.github.com>
2021-10-25 19:53:35 -0300

    Auto Preenchimento de Endereço via CEP
    Auto Preenchimento de Endereço via CEP e ajustes de layout

--- script.js
+++ script.js
@@ -1,22 +1,149 @@
-function cadastro() {
-    //Variaveis obrigatorias
-    var no = document.getElementById('txtnom')
-    var cpf = document.getElementById('txtcpf')
-    var end = document.getElementById('txtend')
-    var num = document.getElementById('txtnum')
-    var cel = document.getElementById('txtcel')
-    //Outras variaveis
-    var rg = document.getElementById(`txtrg`)
-    var sexm = document.getElementById(`masc`)
-    var sexf = document.getElementById(`fem`)
-    var bair = document.getElementById(`txtbair`)
-    var cid = document.getElementById(`txtcid`)
-    var est = document.getElementById(`txtest`)
-    var cep = document.getElementById(`txtcep`)
-    var tel = document.getElementById(`txtfixo`)
-    //Variavel resultado
-    var res = document.getElementById(`resultado`)
+//Variaveis campos obrigatorios
+var no = document.getElementById('txtnom')
+var cpf = document.getElementById('txtcpf')
+var end = document.getElementById('txtend')
+var num = document.getElementById('txtnum')
+var cel = document.getElementById('txtcel')
+//Outras variaveis
+var rg = document.getElementById(`txtrg`)
+var sexm = document.getElementById(`masc`)
+var sexf = document.getElementById(`fem`)
+var bair = document.getElementById(`txtbair`)
+var cid = document.getElementById(`txtcid`)
+var est = document.getElementById(`txtest`)
+var cep = document.getElementById(`txtcep`)
+var tel = document.getElementById(`txtfixo`)
+//Variavel resultado
+var res = document.getElementById(`resultado`)
 
+//Mascaras *
+
+//Mascara CPF
+function mascaraCpf() {
+    var cpf = document.getElementById('txtcpf')
+    if (cpf.value.length == 3 || cpf.value.length == 7) {
+        cpf.value += "."
+    } else if (cpf.value.length == 11) {
+        cpf.value += "-"
+    }
+}
+
+//Mascara CEP
+function mascaraCep() {
+    var cep = document.getElementById(`txtcep`)
+    if (cep.value.length == 5) {
+        cep.value += "-"
+    }
+}
+
+//Mascara RG
+function mascaraRg() {
+    var rg = document.getElementById(`txtrg`)
+    if (rg.value.length == 2 || rg.value.length == 6) {
+        rg.value += "."
+    } else if (rg.value.length == 10) {
+        rg.value += "-"
+    }
+}
+
+//Mascara Telefone
+function mascaraTel() {
+    var tel = document.getElementById(`txtfixo`)
+    if (tel.value.length == (-0)) {
+        tel.value += "("
+    } else if (tel.value.length == 3) {
+        tel.value += ")"
+    } else if (tel.value.length == 8) {
+        tel.value += "-"
+    }
+}
+
+//Mascara Celular
+function mascaraCel() {
+    var cel = document.getElementById('txtcel')
+    if (cel.value.length == (-0)) {
+        cel.value += "("
+    } else if (cel.value.length == 3) {
+        cel.value += ")"
+    } else if (cel.value.length == 9) {
+        cel.value += "-"
+    }
+
+}
+
+//Auto Preenchimento de Endereço via CEP *
+
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('txtend').value = ("");
+    document.getElementById('txtbair').value = ("");
+    document.getElementById('txtcid').value = ("");
+    document.getElementById('txtest').value = ("");
+
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('txtend').value = (conteudo.logradouro);
+        document.getElementById('txtbair').value = (conteudo.bairro);
+        document.getElementById('txtcid').value = (conteudo.localidade);
+        document.getElementById('txtest').value = (conteudo.uf);
+
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
+    }
+}
+
+function pesquisacep(valor) {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('txtend').value = "...";
+            document.getElementById('txtbair').value = "...";
+            document.getElementById('txtcid').value = "...";
+            document.getElementById('txtest').value = "...";
+
+
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    } //end if.
+    else {
+        //cep sem valor, limpa formulário.
+        limpa_formulário_cep();
+    }
+};
+
+
+//função do botão de cadastro *
+function cadastro() {
     // Converter em número
     /* var ncpf = Number(cpf.value)
     var nnum = Number(num.value)
@@ -93,59 +220,5 @@ function cadastro() {
         res.innerHTML += `<p><strong>Cidade: </strong>${cid.value} <strong>Estado: </strong>${est.value}</p>`
         res.innerHTML += `<p><strong>CEP: </strong>${cep.value}</p>`
         res.innerHTML += `<p><strong>Telefone: </strong>${tel.value} <strong>Celular: </strong>${cel.value}</p>`
-    }
-}
-
-//Mascaras:
-
-//Mascara CPF
-function mascaraCpf() {
-    var cpf = document.getElementById('txtcpf')
-    if (cpf.value.length == 3 || cpf.value.length == 7) {
-        cpf.value += "."
-    } else if (cpf.value.length == 11) {
-        cpf.value += "-"
-    }
-}
-
-//Mascara CEP
-function mascaraCep() {
-    var cep = document.getElementById(`txtcep`)
-    if (cep.value.length == 5) {
-        cep.value += "-"
-    }
-}
-
-//Mascara RG
-function mascaraRg() {
-    var rg = document.getElementById(`txtrg`)
-    if (rg.value.length == 2 || rg.value.length == 6) {
-        rg.value += "."
-    } else if (rg.value.length == 10) {
-        rg.value += "-"
-    }
-}
-
-//Mascara Telefone
-function mascaraTel() {
-    var tel = document.getElementById(`txtfixo`)
-    if (tel.value.length == (-0)) {
-        tel.value += "("
-    } else if (tel.value.length == 3) {
-        tel.value += ")"
-    } else if (tel.value.length == 8) {
-        tel.value += "-"
-    }
-}
-
-//Mascara Celular
-function mascaraCel() {
-    var cel = document.getElementById('txtcel')
-    if (cel.value.length == (-0)) {
-        cel.value += "("
-    } else if (cel.value.length == 3) {
-        cel.value += ")"
-    } else if (cel.value.length == 9) {
-        cel.value += "-"
     }
 }
